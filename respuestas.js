9b9587c9113f5ccd6e74d930acf113cb87b3c184
annaescofet <440314.kostka@fje.edu>
2025-09-25 19:56:40 +0200

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,5 +1,5 @@
 function resultado() {
-    var p1=0, p2=0, p3=0, p4=0, p5=0, p6=0, p7=0, p8=0, nota;
+    var p1=0, p2=0, p3=0, p4=0, p5=0, p6=0, p7=0, p8=0,p9=0,p10=0, nota;
 
     // 1a pregunta
     if (document.getElementById('p11').checked) { p1 += 1; }
@@ -33,11 +33,17 @@ function resultado() {
     if (document.getElementById('p82').checked) { p8 += 1; }
     if (document.getElementById('p83').checked) { p8 += 0.5; }
 
-    nota = p1+p2+p3+p4+p5+p6+p7+p8;
+    if (document.getElementById('p92').checked) { p9 += 1; }
+    if (document.getElementById('p91').checked) { p9 += 0.5; }
 
-   if (nota >= 5.5) {
+    if (document.getElementById('p102').checked) { p10 += 1; }
+    if (document.getElementById('p101').checked) { p10 += 0.5; }
+
+    nota = p1+p2+p3+p4+p5+p6+p7+p8+p9+p10;
+
+   if (nota >= 7.0) {
     window.location = 'resultados6.html?nota=' + nota;
-} else if (nota >=3) {
+} else if (nota >=4) {
     window.location = 'resultados4.html?nota=' + nota;
 
 } else {
